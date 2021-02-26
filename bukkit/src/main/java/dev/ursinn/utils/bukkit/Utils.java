66864c59851b4ddf41be30067a767ebca775ee6f
Ursin Filli <git@ursinn.dev>
2021-02-26 21:22:20 +0100

    Add Utils

---
+++ bukkit/src/main/java/dev/ursinn/utils/bukkit/Utils.java
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Ursin Filli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package dev.ursinn.utils.bukkit;
+
+import com.google.common.reflect.ClassPath;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+
+/**
+ * @author Ursin Filli
+ * @version 1.0
+ * @since 1.0
+ */
+public class Utils {
+
+    private Utils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void registerListener(@Nonnull String packageName, @Nonnull Plugin plugin) {
+        PluginManager pluginManager = Objects.requireNonNull(plugin).getServer().getPluginManager();
+        try {
+            for (ClassPath.ClassInfo classInfo : ClassPath.from(ClassLoader.getSystemClassLoader())
+                    .getTopLevelClasses(Objects.requireNonNull(packageName))) {
+                Class<Listener> clazz = (Class<Listener>) Class.forName(classInfo.getName());
+                if (Listener.class.isAssignableFrom(clazz)) {
+                    pluginManager.registerEvents(clazz.getDeclaredConstructor().newInstance(), plugin);
+                }
+            }
+        } catch (IOException | ClassNotFoundException | NoSuchMethodException |
+                IllegalAccessException | InvocationTargetException | InstantiationException exception) {
+            plugin.getLogger().warning(String.valueOf(exception));
+        }
+    }
+}
