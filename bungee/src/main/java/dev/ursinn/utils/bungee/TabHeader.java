926d6f13be0d725b14b2fce0a2097224e94c8eb0
Ursin Filli <git@ursinn.dev>
2021-02-28 17:48:36 +0100

    Add TabHeader

---
+++ bungee/src/main/java/dev/ursinn/utils/bungee/TabHeader.java
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Ursin Filli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package dev.ursinn.utils.bungee;
+
+import lombok.Getter;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.apiguardian.api.API;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+/**
+ * @author Ursin Filli
+ * @version 1.0
+ * @since 1.0
+ */
+@API(status = API.Status.MAINTAINED, since = "1.0")
+public class TabHeader {
+
+    @Getter
+    private final String header;
+
+    @Getter
+    private final String footer;
+
+    /**
+     * Constructor.
+     *
+     * @param header Header line
+     * @param footer Footer line
+     */
+    public TabHeader(@Nonnull String header, @Nonnull String footer) {
+        this.header = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(header));
+        this.footer = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(footer));
+    }
+
+    /**
+     * Send TabHeader to Player
+     *
+     * @param player Player
+     */
+    public void send(@Nonnull ProxiedPlayer player) {
+        player.resetTabHeader();
+        player.setTabHeader(new TextComponent(header), new TextComponent(footer));
+    }
+}
