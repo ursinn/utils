94ca076769f2b74bc14e8a0370f5f4a462def637
Ursin Filli <git@ursinn.dev>
2022-01-15 15:36:03 +0100

    Update Copyright

--- bukkit/bukkit-world/src/main/java/dev/ursinn/utils/bukkit/world/generator/CleanWorldGenerator.java
+++ bukkit/bukkit-world/src/main/java/dev/ursinn/utils/bukkit/world/generator/CleanWorldGenerator.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Ursin Filli
+ * Copyright (c) 2021-2022 Ursin Filli
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
 
 package dev.ursinn.utils.bukkit.world.generator;
